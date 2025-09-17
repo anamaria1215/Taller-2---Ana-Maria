d30f041202ab23f3dad842dfc9f82c148d006c04
Ana María Vargas Mejía <149446905+anamaria1215@users.noreply.github.com>
2025-09-16 23:31:13 -0500

    Ejercicio 1 – Ana María

---
+++ Ejercicio 1 – Tipos de variables.js
@@ -0,0 +1,9 @@
+// Ejercicio 1 - Tipos de variables
+let nombre = "Ana";               // texto
+let edad = 17;                    // número
+let teGustaProgramar = true;      // booleano
+
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("¿Te gusta programar?:", teGustaProgramar);
+
