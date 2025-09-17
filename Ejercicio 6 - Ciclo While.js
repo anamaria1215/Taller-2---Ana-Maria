75907ee5b0e9e519cc8a37b558d56209ae6b8790
Ana María Vargas Mejía <149446905+anamaria1215@users.noreply.github.com>
2025-09-17 01:20:52 -0500

    Ejercicio 6 - Ana

---
+++ Ejercicio 6 - Ciclo While.js
@@ -0,0 +1,15 @@
+// Ejercicio 6 - Ciclo While
+
+// Cuántas veces queremos decir "Hola"
+let cantidad = 5;
+
+// Un contador que empieza en 0
+let contador = 0;
+
+// Mientras el contador sea menor que la cantidad...
+while (contador < cantidad) {
+    console.log("Hola");
+
+    // Aumentamos el contador en 1
+    contador++;
+}
